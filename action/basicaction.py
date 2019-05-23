3413b360f0126053979832d8793985b0dc59ad03
Sanyanghan <sanyanghan@pateo.com.cn>
2019-05-23 16:16:28 +0800

    update appPackage and appActivity

--- action/basicaction.py
+++ action/basicaction.py
@@ -2,6 +2,8 @@
 from appium import webdriver
 import os
 import time
+import subprocess
+import re
 
 class BasicAction():
     def __init__(self, appium_port, udid):
@@ -10,20 +12,31 @@ class BasicAction():
         self.desired_cups['platformName'] = 'Android'
         #设备名称
         self.desired_cups['deviceName'] = 'pateo'
+        apk_activity = os.popen('adb -s {udid} shell dumpsys activity | grep "mFocusedActivity"'.format(udid = udid))
+        activity_read = apk_activity.read()
+        app_package = re.search('\s(com[\s\S]*?)/', activity_read)
+        if app_package != None:
+            app_package = app_package.group(1)
+        app_activity = re.search('(/[\s\S]*?)\s', activity_read)
+        if app_package != None:
+            app_activity = app_package.group(1)
+        if app_package == None:
+            print(os.path.join(os.getcwd(), 'node_modules', 'io.appium.settings', 'apks', 'settings_apk-debug.apk'))
+            self.desired_cups['app'] = os.path.join(os.getcwd(), 'node_modules', 'io.appium.settings', 'apks',
+                                                    'settings_apk-debug.apk')
+        else:
+            self.desired_cups['appPackage'] = app_package
+            self.desired_cups['appActivity'] = app_activity
 
-        self.desired_cups['appPackage'] = 'com.qinggan.app.setting'
-        self.desired_cups['appActivity'] = 'com.qinggan.app.setting.activity.MainActivity'
-        PATH = lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
-        script_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-        # self.desired_cups['app'] = PATH('D:\\pyworkspace\\pateo_test\\apk\\app-debug.apk')
-        print(os.path.join(script_path, '..', 'apk', 'app-debug.apk'))
-        # self.desired_cups['app'] = os.path.join(script_path, '..', 'apk', 'app-debug.apk')
+
+        self.udid = udid
         self.desired_cups['udid'] = udid
         self.desired_cups['autoLaunch'] = False
+        # self.desired_cups["unicodeKeyboard"] = True
+        # self.desired_cups["resetKeyboard"] = True
         self.serive_port = appium_port
-        # desired_cups
 
-        #启动app
+        #启动
         self.driver = webdriver.Remote('http://localhost:%s/wd/hub'%(self.serive_port),self.desired_cups)
 
         #启动app时，需要一定时间进入引导页，所以必须设置等待时间，不然下面会一直报错定位不到元素
@@ -50,7 +63,7 @@ class BasicAction():
             return False
     def get_child(self, id):
         pass
-    
+
     def find_by_uiautomator(self, string):
         """new UiSelector().resourceId("com.qinggan.app.radio:id/freq_tv").childSelector(new UiSelector().className("android.widget.TextView").index(0))"""
         self.driver.find_element_by_android_uiautomator(string)
@@ -65,7 +78,7 @@ class BasicAction():
             return False
 
     def swipe(self, start_x, start_y, end_x, end_y):
-        self.driver.swipe(start_x, start_y, end_x, end_y)
+        self.driver.swipe(start_x, start_y, end_x, end_y, 500)
 
     def home(self):
         self.driver.press_keycode(3)
@@ -73,7 +86,48 @@ class BasicAction():
     def back(self):
         self.driver.back()
 
+    def check_key_in_logcat(self, key):
+        adb_shell_pipe = subprocess.Popen('adb -s {id} logcat | grep {key}'.format(key=key, id=self.udid), stdout=subprocess.PIPE, shell=True)
 
+    def start_catch_logcat(self):
+        timestamp = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time()))
+        os.system('adb -s % logcat -c' % self.udid)
+
+        adb_shell_pipe = subprocess.Popen('adb -s %s shell' % self.udid, stdout=subprocess.PIPE, shell=True)
+        subprocess.Popen('echo %s > /data/logs/logcat.log' % timestamp, stdin=adb_shell_pipe.stdout,
+                                  stdout=subprocess.PIPE)
+        subprocess.Popen('echo %s > /data/logs/top.log' % timestamp, stdin=adb_shell_pipe.stdout,
+                         stdout=subprocess.PIPE)
+        subprocess.Popen('logcat –v threadtime *:V >> /data/logs/logcat.log 2>&1 &', stdin=adb_shell_pipe.stdout,
+                         stdout=subprocess.PIPE)
+        subprocess.Popen('top -d 20 -n 10000 -b >> /data/logs/top.log 2>&1 &', stdin=adb_shell_pipe.stdout,
+                         stdout=subprocess.PIPE)
+        time.sleep(1)
+
+
+    def stop_catch_logcat(self):
+        self.stop_process_on_device('logcat')
+        self.stop_process_on_device('top')
+        log_path = os.path.join('', '')
+
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+        os.system('adb -s %s push /data/logs/logcat.log %s' % (self.udid, os.path.join(log_path, 'logcat.log')))
+        os.system('adb -s %s push /data/logs/top.log %s' % (self.udid, os.path.join(log_path, 'top.log')))
+
+
+    def stop_process_on_device(self, process_name):
+        pid_expression = re.compile(r' \d+')
+        adb_shell_pipe = subprocess.Popen('adb -s %s shell' %self.udid, stdout=subprocess.PIPE, shell=True)
+        result =  subprocess.Popen('ps | grep %s | grep -v grep' % process_name, stdin=adb_shell_pipe.stdout,
+                         stdout=subprocess.PIPE)
+        for line in result.stdout.readlines():
+            for re_result in pid_expression.finditer(str(line)):
+                pid = re_result.group()
+                subprocess.Popen('kill -9 %s' % pid, stdin=adb_shell_pipe.stdout,
+                                 stdout=subprocess.PIPE)
+                break
+            time.sleep(1)
 
     def test(self):
         self.click_by_text('导航')
